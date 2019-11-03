6d69ad59f3ac6c0189c5f8838e009bc1067abc25
JustinKozlowski <jjkozlow@buffalo.edu>
2019-11-02 22:28:14 -0400

    added bfs and dist calculator.

--- METsMath.py
+++ METsMath.py
@@ -1,6 +1,20 @@
+def bfs(grid, start):
+    queue = collections.deque([[start]])
+    seen = set([start])
+    while queue:
+        path = queue.popleft()
+        x, y = path[-1]
+        if grid[y][x] == goal:
+            return path
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            if 0 <= x2 < width and 0 <= y2 < height and grid[y2][x2] != wall and (x2, y2) not in seen:
+                queue.append(path + [(x2, y2)])
+                seen.add((x2, y2))
+
 def nodeDist(Node1, Node2):
-    #get path between nodes
-    unit = #figure out unit
+    path = bfs(grid, Node1, Node2)
+    dist = len(path)
+    return dist
 
 
 def GetMets(carttype, weight):
@@ -17,6 +31,6 @@ def MetsMath(Node1, Node2, carttype, weight):
         METs = GetMets(carttype, weight)
         MetsValue = weight *0.453592
         dist = nodeDist(Node1, Node2)
-        time = dist/#speed make in hours
+        time = dist/1609.34
         caloriesburned = Mets*MetsValue*time
         return caloriesburned

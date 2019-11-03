84f5422afc1d8f3edb189b4d4759dac2aedfdcab
JustinKozlowski <jjkozlow@buffalo.edu>
2019-11-03 06:52:12 -0500

    changed sockt.js io.

--- site/public/javascripts/socket.js
+++ site/public/javascripts/socket.js
@@ -1,14 +1,10 @@
-var socket = require('socket.io-client')('http://localhost:8110', {transports:['websocket']});
- //transports: ['websocket']
+var socket = io.connect({transports: ['websocket']});
 socket.on('connect', function (event) {
-    console.log("connected")
-//    socket.emit("register", "testing", JSON.stringify({"test":True})
+    // console.log("connected")
 });
 socket.on('message', function (event) {
     // received a message from the server
     // console.log(event);
     message = JSON.parse(event)
-    console.log(message)
+    console.log(messaage)
 });
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
